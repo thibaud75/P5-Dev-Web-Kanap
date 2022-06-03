3c3d53f271fa9acf1faddbdb2077d7fd83aa2318
thibaud75 <81164388+thibaud75@users.noreply.github.com>
2022-06-03 13:35:26 +0200

    product + cart

--- front/js/script.js
+++ front/js/script.js
@@ -1,11 +1,22 @@
 const createCanap = (canap) => {
   let canapLink = document.createElement("a");
+  canapLink.href = `product.html?productid=${canap._id}`;
   document.querySelector(".items").appendChild(canapLink);
   let canapArticle = document.createElement("article");
   canapLink.appendChild(canapArticle);
   let canapImg = document.createElement("img");
   canapImg.src = canap.imageUrl;
+  canapImg.alt = canap.altTxt;
   canapArticle.appendChild(canapImg);
+  let canapTitle = document.createElement("h3");
+  canapTitle.innerHTML = canap.name;
+  canapArticle.appendChild(canapTitle);
+  let canapDesc = document.createElement("p");
+  canapDesc.innerHTML = canap.description;
+  canapArticle.appendChild(canapDesc);
+  let canapPrice = document.createElement("p");
+  canapPrice.innerHTML = canap.price + " €";
+  canapArticle.appendChild(canapPrice);
 };
 const createCanaps = (canaps) => {
   for (const canap of canaps) {
